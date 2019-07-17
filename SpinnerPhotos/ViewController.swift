09bb71ecde5e484880e4b88a9ae9b609516d115f
Ri Zhao <bill.zhao24@gmail.com>
2019-07-17 19:36:25 -0400

    first commit

---
+++ SpinnerPhotos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpinnerPhotos
+//
+//  Created by Ri Zhao on 2019-07-17.
+//  Copyright © 2019 Ri Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

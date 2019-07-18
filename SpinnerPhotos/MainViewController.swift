e484b26cce7a4d11dc70818f9a8aa73280f3c4ff
Ri Zhao <bill.zhao24@gmail.com>
2019-07-18 01:16:26 -0400

    added in button tapping logic, as well as degrees label. When tapped, button will spin randomly now functional.

--- SpinnerPhotos/MainViewController.swift
+++ SpinnerPhotos/MainViewController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+/*
+ P1) Make an app with a wheel in the middle of the screen, with a button at the top of the wheel. The button should display where it is on the wheel in degrees(0-360). The user should be able to drag the button around the wheel, and the button text should update to show its new value. Pressing the button will cause the button to rotate around the wheel a random amount, again updating it's displayed value.
+ 
+ P2) Pick a number between 10-20(we'll call it X). Load X random images from the users camera roll into a queue at the bottom of the screen. When the user presses the button in the wheel, as the button rotates around the wheel, the background of the app should display the image in the queue which correlates with the current position of the button. So if X = 10, and if the button has a current value of 0 degrees, the first image in the queue should display as the background image. When the button value changes to 36, the background image should change to the second image in the queue. Every time the user presses the button, X should regenerate and a new selection of images should be loaded
+ 
+ */
+
 class MainViewController: UIViewController {
 
     @IBOutlet weak var spinnerView: UIView!{
@@ -15,8 +22,27 @@ class MainViewController: UIViewController {
             self.drawBezierPath()
         }
     }
+
     
-    @IBOutlet weak var spinnerButtonView: UIView!
+    @IBOutlet weak var spinnerButtonView: UIView!{
+        didSet{
+            let label = UILabel(frame: spinnerButtonView.bounds)
+            spinnerButtonView.addSubview(label)
+            label.textColor = .white
+            label.textAlignment = .center
+            
+            spinnerButtonView.clipsToBounds = false
+            spinnerButtonView.bounds = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 100, height: 50))
+            spinnerButtonView.layer.cornerRadius = 5.0
+            
+            let dragPan = UIPanGestureRecognizer(target: self, action: #selector(dragEmotionOnBezier(recognizer:)))
+            spinnerButtonView.addGestureRecognizer(dragPan)
+            
+            let tap = UITapGestureRecognizer(target: self, action: #selector(spinnerButtonTapped(recognizer:)))
+            spinnerButtonView.addGestureRecognizer(tap)
+            tap.require(toFail: dragPan)
+        }
+    }
     
     var centerPoint: CGPoint! {
         didSet{
@@ -26,11 +52,18 @@ class MainViewController: UIViewController {
     
     var circle : Circle!
     
+    var currentDegree: Int!{
+        didSet{
+            for view in self.spinnerButtonView.subviews{
+                if let label = view as? UILabel{
+                    label.text = "\(String(currentDegree!))"
+                }
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let dragPan = UIPanGestureRecognizer(target: self, action: #selector(dragEmotionOnBezier(recognizer:)))
-        self.spinnerButtonView.addGestureRecognizer(dragPan)
     }
     
     func drawBezierPath() {
@@ -43,25 +76,54 @@ class MainViewController: UIViewController {
         shapeLayer.fillColor = UIColor.blue.cgColor
         shapeLayer.strokeColor = UIColor.green.cgColor
         shapeLayer.lineWidth = 3.0
-        self.spinnerView.layer.addSublayer(shapeLayer)
+        self.spinnerView.layer.insertSublayer(shapeLayer, below: self.spinnerButtonView.layer)
         spinnerButtonView.translatesAutoresizingMaskIntoConstraints = false
         
         self.centerPoint = CGPoint(x: self.spinnerView.bounds.width * 0.5, y: self.spinnerView.bounds.height * 0.5 - self.spinnerView.bounds.width * 0.4)
+        self.currentDegree = 270
         self.circle = Circle(top: CGPoint(x: centerPoint.x, y: centerPoint.y - radius), right: CGPoint(x: centerPoint.x + radius, y: centerPoint.y), bottom: CGPoint(x: centerPoint.x, y: centerPoint.y + radius), left: CGPoint(x: centerPoint.x - radius, y: centerPoint.y), origin: centerPoint, radius: radius)
     }
 
     
     @objc func dragEmotionOnBezier(recognizer: UIPanGestureRecognizer) {
-        
         let point = recognizer.location(in: self.spinnerView)
-        let newPoint = self.circle.getPointInRelationToAngle(with: point)
-        self.centerPoint = newPoint
+        let circleData = self.circle.getPointInRelationToAngle(with: point)
         
+        self.currentDegree = circleData.degree
+        
+        self.centerPoint = circleData.point
+    }
+
+    @objc func spinnerButtonTapped(recognizer: UITapGestureRecognizer) {
+        self.animateButtonRandomly()
     }
     
-    func getLocationOnPath(for point: CGPoint) -> CGPoint{
-        return CGPoint()
-    }
+    func animateButtonRandomly(){
+        let centerPoint = CGPoint(x: self.spinnerView.bounds.width * 0.5, y: self.spinnerView.bounds.height * 0.5)
+        let radius = self.spinnerView.bounds.width * 0.4
+        let randomDegree = Int.random(in: 0..<360)
+        let circlePath = UIBezierPath(arcCenter: centerPoint, radius: radius, startAngle: CGFloat(currentDegree.degreeToRad()), endAngle:CGFloat(randomDegree.degreeToRad()), clockwise: true)
+        
+        CATransaction.begin()
+        CATransaction.setCompletionBlock {
+            self.centerPoint = self.circle.getPointWithDegree(randomDegree)
+            self.currentDegree = randomDegree
+        }
+        
+        let animation = CAKeyframeAnimation(keyPath: "position")
+        animation.path = circlePath.cgPath
+        // I set this one to make the animation go smoothly along the path
+        animation.calculationMode = CAAnimationCalculationMode.paced
+        animation.duration = 1.5
+        self.spinnerButtonView.layer.add(animation, forKey: nil)
+        self.centerPoint = self.circle.getPointWithDegree(randomDegree)
 
+        CATransaction.commit()
+    }
 }
 
+extension Int{
+    func degreeToRad() -> Double{
+        return Double(self) * Double.pi / 180
+    }
+}
--- SpinnerPhotos/circle.swift
+++ SpinnerPhotos/circle.swift
@@ -18,15 +18,24 @@ struct Circle {
     var origin : CGPoint
     var radius : CGFloat
     
-    func getPointInRelationToAngle(with point:CGPoint) -> CGPoint{
-        let v2 = CGVector(dx: bottom.x - origin.x, dy: bottom.y - origin.y)
-        let v1 = CGVector(dx: point.x - origin.x, dy: point.y - origin.y)
+    func getPointInRelationToAngle(with point:CGPoint) -> (point: CGPoint, degree: Int){
+        //calculate angle between the bottom point and the currently selected point
+        //use the radians and radius to calculate for new x, y
+        //translate the x, y to the view's bounds by applying the origin
+        let v1 = CGVector(dx: right.x - origin.x, dy: right.y - origin.y)
+        let v2 = CGVector(dx: point.x - origin.x, dy: point.y - origin.y)
         let angle = atan2(v2.dy, v2.dx) - atan2(v1.dy, v1.dx)
-  
-        let x = radius * sin(angle) + origin.x
-        let y = radius * cos(angle) + origin.y
+        var deg = angle * CGFloat(180.0 / Double.pi)
+        if deg < 0 { deg += 360.0 }
+        print(deg)
+        let x = radius * cos(angle) + origin.x
+        let y = radius * sin(angle) + origin.y
 
-        return CGPoint(x: x, y: y)
+        return (CGPoint(x: x, y: y), Int(deg))
     }
     
+    func getPointWithDegree(_ degree: Int) -> CGPoint{
+        let angle = CGFloat(degree.degreeToRad())
+        return CGPoint(x: radius * cos(angle) + origin.x, y: radius * sin(angle) + origin.y)
+    }
 }
